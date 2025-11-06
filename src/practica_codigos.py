0e59a3a4fe6291f290673c07953f3cc0ba08fec0
12345-007 <2530167@upv.edu.mx>
2025-11-05 23:50:46 -0600

    feat: actualizacion

---
+++ src/practica_codigos.py
@@ -0,0 +1,52 @@
+a = 5
+b = 10
+
+suma = a + b
+resta = b - a
+multiplicacion = a * b
+divicion = b / a
+
+print("suma, resta, multiplicacion, divicion")
+print(suma)
+print(resta)
+print(multiplicacion)
+print(divicion)
+
+
+# variables 
+
+nombre="Emiliano"
+edad=18
+estatura=1.75
+es_estudiante=True
+
+print("nombre:", nombre)
+print("edad:", edad)
+print("estatura:", estatura)
+print("es_estudiante:", es_estudiante)
+ 
+
+edad= 17
+if edad >= 18:
+    print("es mayor de edad")
+else:
+    print("es menor de edad")
+
+print("calculadora basica")
+a= float(input("ingrsa el primer numero:"))
+b= float(input("ingresa le segundo numero:"))
+
+print("suma:", a+b)
+print("resta:", a+b)
+print("multiplicacion:", a*b)
+print("divicion:", a/b)
+print("modulo:", a%b)
+
+
+
+
+
+
+
+
+
